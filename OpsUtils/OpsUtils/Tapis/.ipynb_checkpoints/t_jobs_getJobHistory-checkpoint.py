409c1bc3154bda4ca6301094d39b05bdac95b3d0
silviamazzoni <silviamazzoni@yahoo.com>
2025-08-21 04:28:52 +0000

    removed ipynbs

--- OpsUtils/OpsUtils/Tapis/.ipynb_checkpoints/t_jobs_getJobHistory-checkpoint.py
+++
@@ -1,7 +0,0 @@
-def t_jobs_getJobHistory(t,jobUuid):
-    try: 
-        JobHistory = t.jobs.getJobHistory(jobUuid=jobUuid)
-        return JobHistory
-    except Exception as e:
-        print(e)
-        return -1
